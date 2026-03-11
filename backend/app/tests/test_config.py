a3796f4b43e6dab46b547baa0bc746ac884e2d36
julien <forevergoodforest@outlook.com>
2026-03-11 17:27:24 +0800

    chore: 更改pytest.ini设置

--- backend/app/tests/test_config.py
+++ backend/app/tests/test_config.py
@@ -5,4 +5,4 @@ def test_secret_key_is_loaded():
     print(f"\n[DEBUG] 内存中的 Secret Key: {settings.secret_key}")
 
     assert settings.secret_key is not None
-    assert settings.secret_key != "nothing"
+    assert settings.secret_key != ""
